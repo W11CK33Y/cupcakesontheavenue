aca8c9ff46530a6fadd7e07d6c33fac13e59ee7a
W11CK33Y <thejjwicks@gmail.com>
2025-11-22 22:34:19 +0000

    modified:   discord-chat.js 	modified:   index.html

--- discord-chat.js
+++ discord-chat.js
@@ -19,10 +19,15 @@ class DiscordChatWidget {
   }
 
   init() {
+    console.log('🎯 Chat widget init() called');
     this.injectStyles();
+    console.log('✅ Styles injected');
     this.createWidget();
+    console.log('✅ Widget created');
     this.attachEventListeners();
+    console.log('✅ Event listeners attached');
     this.loadWelcomeMessage();
+    console.log('✅ Welcome message loaded');
   }
 
   injectStyles() {
@@ -41,7 +46,6 @@ class DiscordChatWidget {
     widget.innerHTML = `
       <button class="chat-button" id="chatToggleBtn" aria-label="Open chat">
         <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="white">
-          <!-- Live chat bubble icon -->
           <path d="M20 2H4c-1.1 0-2 .9-2 2v18l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm0 14H6l-2 2V4h16v12z"/>
           <circle cx="8" cy="10" r="1.5"/>
           <circle cx="12" cy="10" r="1.5"/>
